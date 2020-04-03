4979dbac5a548ca6c23b5cb00d6b215294dc63c0
Stryse <tianarath30@gmail.com>
2020-04-03 23:07:26 +0200

    main.cpp refactored

--- src/main.cpp
+++ src/main.cpp
@@ -2,70 +2,87 @@
 #include "student.h"
 #include "schoolClass.h"
 
-const char* filePath = "input.txt";
+void listStudents(const std::string& filePath);
+int countClasses(const std::string& filePath);
+void handleSPaperDataEnorError(const StudentPaperDataEnor::Errors& e, const std::string& filePath);
 
+#define NORMAL_MODE
+#ifdef NORMAL_MODE
 
 int main()
 {
+    std::cout << "Please enter the name of the input file: ";
+    std::string filePath;
+    std::cin >> filePath;
     // First Task
     try
-    {   std::cout << "First Task" << std::endl;
-        StudentPaperDataEnor t(filePath);
-        for(t.first(); !t.end(); t.next())
-        {
-            if(t.current().hasMoreThan5kg)
-            {
-                std::cout << t.current().name << " " << t.current().className << std::endl;
-            }
-        }
-    }
-    catch(StudentPaperDataEnor::Errors& e)
     {
-        switch (e)
-        {
-        case StudentPaperDataEnor::Errors::FILE_ERROR:
-            std::cerr << "Could not open file: " << filePath << ".Exiting..." << std::endl; 
-            break;
-
-        case StudentPaperDataEnor::Errors::INVALID_VALUE_READ:
-            std::cerr << "An invalid line has been read from input file" << std::endl;
-            break;
-             
-        default:
-            std::cerr << "Unhandled exception" << std::endl;
-            break;
-        }
+        std::cout << "==============First Task===============" << std::endl;
+        listStudents(filePath);
     }
-    
+    catch (const StudentPaperDataEnor::Errors &e)
+    {
+        handleSPaperDataEnorError(e,filePath);
+    }
 
+    // Second Task
     try
     {
-        SchoolClassEnor t(filePath);
-        int c = 0;
-        for(t.first(); !t.end(); t.next())
-        {
-            if(t.current())
-                ++c;
-        }
-        std::cout << c << std::endl;
+        std::cout << "==============Second Task==============" << std::endl;
+        std::cout << countClasses(filePath) << std::endl;  
     }
-    catch(StudentPaperDataEnor::Errors& e)
+    catch (const StudentPaperDataEnor::Errors &e)
     {
-        switch (e)
-        {
-        case StudentPaperDataEnor::Errors::FILE_ERROR:
-            std::cerr << "Could not open file: " << filePath << ".Exiting..." << std::endl; 
-            break;
-
-        case StudentPaperDataEnor::Errors::INVALID_VALUE_READ:
-            std::cerr << "An invalid line has been read from input file" << std::endl;
-            break;
-             
-        default:
-            std::cerr << "Unhandled exception" << std::endl;
-            break;
-        }
+        handleSPaperDataEnorError(e,filePath);
     }
     
     return 0;
+}
+
+#else
+#include "catch.hpp"
+#define CATCH_CONFIG_MAIN
+#include "tests.h"
+#endif
+
+void listStudents(const std::string& filePath)
+{
+    StudentPaperDataEnor t(filePath);
+    for (t.first(); !t.end(); t.next())
+    {
+        if (t.current().hasMoreThan5kg)
+        {
+            std::cout << t.current().name << " " << t.current().className << std::endl;
+        }
+    }
+}
+
+int countClasses(const std::string& filePath)
+{
+    SchoolClassEnor t(filePath);
+    int c = 0;
+    for (t.first(); !t.end(); t.next())
+    {
+        if (t.current())
+            ++c;
+    }
+    return c;
+}
+
+void handleSPaperDataEnorError(const StudentPaperDataEnor::Errors& e, const std::string& filePath)
+{
+    switch (e)
+    {
+    case StudentPaperDataEnor::Errors::FILE_ERROR:
+        std::cerr << "Could not open file: \"" << filePath << "\" Exiting..." << std::endl;
+        break;
+
+    case StudentPaperDataEnor::Errors::INVALID_VALUE_READ:
+        std::cerr << "An invalid line has been read from input file" << std::endl;
+        break;
+
+    default:
+        std::cerr << "Unhandled exception" << std::endl;
+        break;
+    }
 }
